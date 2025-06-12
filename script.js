e5a5e40b3a7a6c1273a12c31212c9869f37b15d6
dcendry1711 <daniel.cendry@gmail.com>
2025-06-12 15:29:29 +0200

    handle rejected promise

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
 	.then(res => res.json())
 	.then(data => {
-        console.log(data)
 		document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById('author-section').textContent = `By: ${data.user.name}`
 	})
+    .catch(err => {
+        console.log('Something went wrong: ',err)
+        document.body.style.backgroundImage = `url(https://seocom.agency/wp-content/uploads/2024/05/Errores-Web-404-403-503-502-401.-Significado-y-soluciones-1.jpg.webp)`
+    })
+
+    //https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
